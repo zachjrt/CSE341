c214e1d8c1df8c35c1360fc435161a2ab2a974da
Zachary Thompson <tho01699@ttu.edu>
2023-01-12 11:47:03 -0600

    working .env

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,3 @@
-<<<<<<< HEAD
-=======
-const routes = require('express').Router();
->>>>>>> e926fc394b9c92105314d3b1e0c82fbf116ac78c
-
-
-
 
 const express = require('express');
 const FriendController = require('../controllers/index');
