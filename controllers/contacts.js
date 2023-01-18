152e4d41c6fe8efba7892f7e605cbac7ad869176
Zachary Thompson <tho01699@ttu.edu>
2023-01-18 16:57:47 -0600

    Commit Working CRUD

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,7 +1,7 @@
 const mongodb = require('../db/connect');
 const ObjectId = require('mongodb').ObjectId;
 
-//all
+//Get all
 const getFriends = async (req, res, next) => {
   const result = await mongodb.getDb().db().collection('contacts').find();
   result.toArray().then((lists) => {
@@ -10,7 +10,7 @@ const getFriends = async (req, res, next) => {
   });
 };
 
-//one based on id
+//GET one based on id
 const getFriend = async (req, res, next) => {
   const userId = new ObjectId(req.params.id);
   const result = await mongodb.getDb().db().collection('contacts').find({ _id: userId });
@@ -20,4 +20,53 @@ const getFriend = async (req, res, next) => {
   });
 };
 
-module.exports = { getFriends, getFriend };
+//Post
+const createFriend = async (req, res, next) => {
+  //Data to be added
+  const userFirstName = req.params.first;
+  const userLastName = req.params.last;
+  const userEmail = req.params.email;
+  const userColor = req.params.color;
+  const userBirthday = req.params.birth;
+  const userData = {'firstName':userFirstName, 'lastName':userLastName, 'email':userEmail, 'favoriteColor':userColor, 'birthday':userBirthday};
+  //Operation
+  const result = await mongodb.getDb().db().collection('contacts')
+                      .insertOne(userData);
+
+  //Response
+
+  //console confirmation
+  console.log(`${result.modifiedCount} document(s) was/were created.`);
+};
+
+//Put Update the Friend based on ID
+const updateFriend = async (req, res, next) => {
+    //ID to update
+    const userId = new ObjectId(req.params.id);
+    //Data to update
+    const userFirstName = req.params.first;
+    const userLastName = req.params.last;
+    const userEmail = req.params.email;
+    const userColor = req.params.color;
+    const userBirthday = req.params.birth;
+    const userData = {'firstName':userFirstName, 'lastName':userLastName, 'email':userEmail, 'favoriteColor':userColor, 'birthday':userBirthday};
+    //operation
+    const result = await mongodb.getDb().db().collection('contacts')
+                        .updateOne({ _id: userId }, {$set: userData});
+    //response
+    console.log(`${result.matchedCount} document(s) matched the query criteria.`);
+    console.log(`${result.modifiedCount} document(s) was/were updated.`);
+};
+//Delete
+const deleteFriend = async (req, res, next) => {
+  //ID to delete
+  const userId = new ObjectId(req.params.id);
+  //Operation
+  const result = await mongodb.getDb().db().collection('contacts')
+                      .deleteOne({ _id: userId });
+  //Response
+  console.log(`${result.matchedCount} document(s) matched the query criteria.`);
+  console.log(`${result.modifiedCount} document(s) was/were updated.`);
+};
+
+module.exports = { getFriends, getFriend, updateFriend, createFriend, deleteFriend };
--- routes/contacts.js
+++ routes/contacts.js
@@ -7,4 +7,10 @@ router.get('/', contactsController.getFriends);
 
 router.get('/:id', contactsController.getFriend);
 
+router.get('/update/:id/:first/:last/:email/:color/:birth', contactsController.updateFriend);
+
+router.get('/create/:first/:last/:email/:color/:birth', contactsController.createFriend);
+
+router.get('/delete/:id', contactsController.deleteFriend);
+
 module.exports = router;
