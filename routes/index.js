b586c0c6f8a0b1d430fca3a1e09254b1d4831f16
Zachary Thompson <tho01699@ttu.edu>
2023-01-26 12:39:51 -0600

    Swagger test

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/contacts', require('./contacts'))
+router.use('/', require('./swagger'));
 
 module.exports = router;
