355ccbd22039632ceca5d97c2beb5787f984582a
Zachary Thompson <tho01699@ttu.edu>
2023-01-26 13:11:30 -0600

    server app update

--- server.js
+++ server.js
@@ -12,10 +12,10 @@ app
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader(
       'Access-Control-Allow-Headers',
-      'Origin, x-Requested-With, Content-Type, Accept, Z-key'
+      'Origin, X-Requested-With, Content-Type, Accept, Z-Key'
     );
     res.setHeader('Content-Type', 'application/json');
-    res.setHeader('Access=Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
   })
   .use('/', FriendRoutes);
